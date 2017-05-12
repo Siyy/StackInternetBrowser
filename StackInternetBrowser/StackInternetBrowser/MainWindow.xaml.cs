4c9325a56291a9840a9e442a8337b2abf8249650
久永 <jiuyong@hotmail.com>
2017-05-12 21:32:47 +0800

    WPF的浏览器仅仅提供很少的功能，甚至无法拦截弹出窗口。

--- StackInternetBrowser/StackInternetBrowser/MainWindow.xaml.cs
+++ StackInternetBrowser/StackInternetBrowser/MainWindow.xaml.cs
@@ -25,5 +25,21 @@ namespace Jiuyong
 		{
 			InitializeComponent();
 		}
+
+		private void WebBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+		{
+
+		}
+
+		private void WebBrowser_Navigated(object sender, NavigationEventArgs e)
+		{
+
+		}
+
+		private void WebBrowser_Navigating(object sender, NavigatingCancelEventArgs e)
+		{
+
+		}
+
 	}
 }
